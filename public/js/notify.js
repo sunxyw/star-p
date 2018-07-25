d8fc728e1259e7e0b067cbbede37a3518a03f394
sunxyw <xy2496419818@gmail.com>
2018-07-25 19:01:47 +0800

    优化notify以适应pjax

--- public/js/notify.js
+++ public/js/notify.js
@@ -1,69 +1,71 @@
-var successi = {
-    class: "alert alert-success",
-    icon: "fas fa-check"
-};
-var warningi = {
-    class: "alert alert-warning",
-    icon: "fas fa-exclamation"
-};
-var infoi = {
-    class: "alert alert-info",
-    icon: "fas fa-info"
-};
-var dangeri = {
-    class: "alert alert-danger",
-    icon: "fas fa-times"
-};
+if (!successi) {
+    var successi = {
+        class: "alert alert-success",
+        icon: "fas fa-check"
+    };
+    var warningi = {
+        class: "alert alert-warning",
+        icon: "fas fa-exclamation"
+    };
+    var infoi = {
+        class: "alert alert-info",
+        icon: "fas fa-info"
+    };
+    var dangeri = {
+        class: "alert alert-danger",
+        icon: "fas fa-times"
+    };
 
-function notify($class, $icon, $msg, $id) {
-    return '<div style="display: none" id="notify' + $id + '" class="' + $class + '" role="alert">\n' +
-        '            <i class="' + $icon + '"></i>\n' + $msg +
-        '            <button type="button" class="close" data-dismiss="alert">\n' +
-        '                <i class="fas fa-times"></i>\n' +
-        '            </button>\n' +
-        '        </div>';
-}
+    function notify($class, $icon, $msg, $id) {
+        return '<div style="display: none" id="notify' + $id + '" class="' + $class + '" role="alert">\n' +
+            '            <i class="' + $icon + '"></i>\n' + $msg +
+            '            <button type="button" class="close" data-dismiss="alert">\n' +
+            '                <i class="fas fa-times"></i>\n' +
+            '            </button>\n' +
+            '        </div>';
+    }
 
-function success($msg) {
-    var id = new Date().getTime();
-    $("#message").append(notify(successi.class, successi.icon, $msg, id));
+    function success($msg) {
+        var id = new Date().getTime();
+        $("#message").append(notify(successi.class, successi.icon, $msg, id));
 
-    $("#notify" + id).slideDown();
+        $("#notify" + id).slideDown();
 
-    setTimeout(function () {
-        $("#notify" + id).slideUp();
-    }, 4000);
-}
+        setTimeout(function () {
+            $("#notify" + id).slideUp();
+        }, 4000);
+    }
 
-function info($msg) {
-    var id = new Date().getTime();
-    $("#message").append(notify(infoi.class, infoi.icon, $msg));
+    function info($msg) {
+        var id = new Date().getTime();
+        $("#message").append(notify(infoi.class, infoi.icon, $msg));
 
-    $("#notify" + id).slideDown();
+        $("#notify" + id).slideDown();
 
-    setTimeout(function () {
-        $("#notify" + id).slideUp();
-    }, 4000);
-}
+        setTimeout(function () {
+            $("#notify" + id).slideUp();
+        }, 4000);
+    }
 
-function warning($msg) {
-    var id = new Date().getTime();
-    $("#message").append(notify(warningi.class, warningi.icon, $msg));
+    function warning($msg) {
+        var id = new Date().getTime();
+        $("#message").append(notify(warningi.class, warningi.icon, $msg));
 
-    $("#notify" + id).slideDown();
+        $("#notify" + id).slideDown();
 
-    setTimeout(function () {
-        $("#notify" + id).slideUp();
-    }, 4000);
-}
+        setTimeout(function () {
+            $("#notify" + id).slideUp();
+        }, 4000);
+    }
 
-function danger($msg) {
-    var id = new Date().getTime();
-    $("#message").append(notify(dangeri.class, dangeri.icon, $msg));
+    function danger($msg) {
+        var id = new Date().getTime();
+        $("#message").append(notify(dangeri.class, dangeri.icon, $msg));
 
-    $("#notify" + id).slideDown();
+        $("#notify" + id).slideDown();
 
-    setTimeout(function () {
-        $("#notify" + id).slideUp();
-    }, 4000);
+        setTimeout(function () {
+            $("#notify" + id).slideUp();
+        }, 4000);
+    }
 }
